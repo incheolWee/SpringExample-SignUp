664e37981e6793852c1611aecef7fd48931a4232
dnldl <dnldlscjf@gmail.com>
2024-02-18 19:53:56 +0900

    views 로 이름 변경 및 model 데이터 정의, viewresolver 설정

--- src/main/java/com/example/config/MVCConfig.java
+++ src/main/java/com/example/config/MVCConfig.java
@@ -1,4 +1,25 @@
 package com.example.config;
 
-public class MVCConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan (basePackages = {"com.example"})
+
+public class MVCConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver resolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class); // 이게 뭐지
+        resolver.setPrefix("/WEB-INF/views");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 }
--- src/main/java/com/example/model/SignUpForm.java
+++ src/main/java/com/example/model/SignUpForm.java
@@ -1,4 +1,14 @@
 package com.example.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SignUpForm {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String userName;
+    private String password;
 }
